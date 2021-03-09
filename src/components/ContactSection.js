c99290f013497911717cd893b0de87471ab9fd34
kandloic <jkand011@uottawa.ca>
2021-03-08 21:32:45 -0500

    Refactored contact section to include optional input fields

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,54 +1,128 @@
 import React from 'react';
+import _ from 'lodash';
+
+import {classNames, toStyleObj, withPrefix} from '../utils';
+import SectionActions from './SectionActions';
 
 export default class ContactSection extends React.Component {
     render() {
-        return (
-            <section className="section">
-              <div className="container container--md">
-                <form name="contactForm" id="contactForm" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-                  <div className="sr-only">
-                    <label id="honeypot-label" htmlFor="honeypot">Don't fill this out if you're human:</label>
-                    <input aria-labelledby="honeypot-label" id="honeypot" name="bot-field" />
+      let section = _.get(this.props, 'section', null);
+      let background = _.get(section, 'background', null);
+      let background_color = _.get(background, 'background_color', null) || 'white';
+      let background_opacity_pct = _.get(background, 'background_image_opacity', null) || 100;
+      let background_opacity = background_opacity_pct * 0.01;
+      let background_size = _.get(background, 'background_image_size', null) || 'cover';
+      let background_repeat = _.get(background, 'background_image_repeat', null) || 'no-repeat';
+
+      return (
+          <section className={classNames('section', 'hero', {'bg-image': _.get(section, 'has_background', null) && _.get(background, 'background_image', null), 'inverse bg-blue': _.get(section, 'has_background', null) && (background_color === 'blue'), 'bg-gray': _.get(section, 'has_background', null) && (background_color === 'gray'), 'section--padding': _.get(section, 'has_background', null) || _.get(section, 'image', null)})}>
+            <div className="container container--lg">
+              <div className={classNames('flex', 'flex--middle', 'flex--center', 'flex--col-2', {'align-center': _.get(section, 'align', null) === 'center', 'align-right': _.get(section, 'align', null) === 'right'})}>
+                
+                {(_.get(section, 'has_background', null) && _.get(background, 'background_image', null)) && (
+                <div className="bg-image__image" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(background, 'background_image', null)) + '\'); opacity: ' + background_opacity + '; background-size: ' + background_size + '; background-repeat: ' + background_repeat)}/>
+                )}
+
+                {_.get(section, 'image', null) && (
+                <div className={classNames('cell', 'section__media', {'section__media--right': _.get(section, 'image_position', null) === 'right'})}>
+                  <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
+                </div>
+                )}
+                
+                <div className="cell section__body">
+                  {_.get(section, 'title', null) && (
+                  <div>
+                    <h1 className="section__title">{_.get(section, 'title', null)}</h1>
                   </div>
-                  <input aria-labelledby="honeypot-label" type="hidden" name="form-name" value="contactForm" />
-                  <div className="form-group">
-                    <label htmlFor="subject">Subject</label>
-                    <div className="form-select-wrap">
-                      <select name="subject" id="subject">
-                        <option value="">Please select</option>
-                        <option value="Business Inquiries">Business Inquiries</option>
-                        <option value="Sponsorship">Partnerships</option>
-                        <option value="Other">Other</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <label id="name-label" htmlFor="name">Name</label>
-                    <input aria-labelledby="name-label" type="text" name="name" id="name" placeholder="Your name" required />
-                  </div>
-                  <div className="form-group">
-                    <label id="email-label" htmlFor="email">Email</label>
-                    <input aria-labelledby="email-label" type="email" name="email" id="email" placeholder="Your email" />
-                  </div>
-                  <div className="form-group">
-                    <label id="phone-label" htmlFor="phone">Phone Number</label>
-                    <input aria-labelledby="phone-label" type="tel" name="tel" id="tel" placeholder="Your phone number" />
-                  </div>                  
-                  <div className="form-group">
-                    <label id="message-label" htmlFor="message">Message</label>
-                    <textarea aria-labelledby="message-label" name="message" id="message" rows="5" placeholder="Your message" />
-                  </div>
-                  <div className="form-group form-checkbox">
-                    <input aria-labelledby="consent-label" type="checkbox" name="consent" id="consent" />
-                    <label id="consent-label" htmlFor="consent">I understand that this form is storing my submitted information so I can be
-                      contacted.</label>
-                  </div>
-                  <div className="form-submit">
-                    <button type="submit" className="button">Submit</button>
-                  </div>
-                </form>
-              </div>
-            </section>
-        );
-    }
+                )}
+
+                {_.get(section, 'subtitle', null) && (
+                <div className="section__copy">
+                  <p>{_.get(section, 'subtitle', null)}</p>
+                </div>
+                )}
+
+                {_.get(section, 'actions', null) && (
+                <div className="section__actions btn-group">
+                  <SectionActions {...this.props} actions={_.get(section, 'actions', null)} />
+                </div>
+                )}
+
+                    {_.get(section, 'has_form', null) && (
+                      <form name="contactForm" id="contactForm" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+                        
+                        <div className="sr-only">
+                          <label id="honeypot-label" htmlFor="honeypot">Don't fill this out if you're human:</label>
+                          <input aria-labelledby="honeypot-label" id="honeypot" name="bot-field" />
+                        </div>
+
+                        <input aria-labelledby="honeypot-label" type="hidden" name="form-name" value="contactForm" />
+
+                        <div className="form-group">
+                          <label id="name-label" htmlFor="name">Name</label>
+                          <input aria-labelledby="name-label" type="text" name="name" id="name" placeholder="Your name" required />
+                        </div>
+
+                        <div className="form-group">
+                          <label id="email-label" htmlFor="email">Email</label>
+                          <input aria-labelledby="email-label" type="email" name="email" id="email" placeholder="Your email" />
+                        </div>
+
+                        <div className="form-group">
+                          <label id="phone-label" htmlFor="phone">Phone Number</label>
+                          <input aria-labelledby="phone-label" type="tel" name="tel" id="tel" placeholder="Your phone number" />
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="subject">Preferred method of contact</label>
+                          <div className="form-select-wrap">
+                            <select name="subject" id="subject">
+                              <option value="">Please select</option>
+                              <option value="Business Inquiries">Email</option>
+                              <option value="Sponsorship">Phone</option>
+                            </select>
+                          </div>
+                        </div>
+
+                        {_.get(section, 'has_university_field', null) && (
+                          <div className="form-group">
+                            <label id="university" htmlFor="phone">University</label>
+                            <input aria-labelledby="university" type="text" name="text" id="university" placeholder="Current Institution" />
+                          </div>
+                        )}
+
+                        {_.get(section, 'has_technologies_field', null) && (
+                          <div className="form-group">
+                            <label id="technologies" htmlFor="phone">Technologies</label>
+                            <input aria-labelledby="technologies" type="text" name="text" id="technologies" placeholder="List all technologies and programming languages you know" />
+                          </div>
+                        )}
+
+                        {_.get(section, 'has_message_field', null) && (
+                          <div className="form-group">
+                          <label id="message-label" htmlFor="message">Message</label>
+                          <textarea aria-labelledby="message-label" name="message" id="message" rows="5" placeholder="Your message" />
+                        </div>
+                        )}
+
+                        <div className="form-group form-checkbox">
+                          <input aria-labelledby="consent-label" type="checkbox" name="consent" id="consent" />
+                          <label id="consent-label" htmlFor="consent">I understand that this form is storing my submitted information so I can be
+                            contacted.</label>
+                        </div>
+
+                        <div className="form-submit">
+                          <button type="submit" className="button">Submit</button>
+                        </div>
+
+                    </form>
+                    
+                    )}
+
+                 </div>
+               </div>
+             </div>
+           </section>
+       );
+   }
 }
--- src/pages/non-profits.md
+++ src/pages/non-profits.md
@@ -5,23 +5,25 @@ sections:
     title: Our Values
     subtitle: >-
       Join Software for Love as a non-profit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat rhoncus bibendum sed semper turpis a id a at. Dui, nunc at mauris maecenas leo enim. Ante accumsan mattis hac ullamcorper amet eleifend quisque et. Sed bibendum interdum aliquam vel. Adipiscing.
-    actions:
-      - label: Get in touch
-        url: /contact
-        style: primary
-      - label: Volunteer
-        url: /volunteers
-        style: secondary
     align: left
-    image: images/splash.png
+    image: images/progressive_app.svg
     image_alt: Hero placeholder image
     image_position: right
     has_background: true
     background:
-      background_color: blue
-  - type: hero_section
-    title: Leave us your info and we will get back to you
-    align: left
+      background_color: white
   - type: contact_section
+    title: Contact Us
+    subtitle: Got a question, need help with a problem or just want to leave feedback? Leave us your contact info and we will get back to you.
+    has_form: true
+    has_message_field: true
+    # has_university_field: true
+    # has_technologies_field: true
+    image: images/progressive_app.svg
+    image_alt: Hero placeholder image
+    image_position: right
+    has_background: true
+    background:
+      background_color: gray
 template: advanced
 ---
