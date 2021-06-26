0fbbc28330a0b7df2ccd4ab7db5b8fa0a338c047
sultanoloyede <sultanoloyede@hotmail.com>
2021-06-26 12:27:44 -0400

    Changed font and padding of cookie banner button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -33,9 +33,10 @@ export default class Body extends React.Component {
                     location="bottom"
                     buttonText="Accept"
                     declineButtonText="Decline"
+                    font-family = "Segoe UI"
                     cookieName="gatsby-gdpr-google-analytics"
                     style={{ background: "#9BA9D0" }}
-                    buttonStyle={{ background: "#FFFFFF", color:"#394A74", fontSize: "18px", borderRadius: '5px' }}>
+                    buttonStyle={{ background: "#FFFFFF", color:"#394A74", fontSize: "1em", borderRadius: '5px', padding: "0.625em 1.5em" }}>
                     We value your privacy. We use cookies to give you the best online experience and to better understand our visitors. To find out more, please read our <a href="https://www.softwareforlove.com/terms" target="_blank" rel="noopener noreferrer"> privacy policy</a>.
                 </CookieConsent>
             </React.Fragment>
