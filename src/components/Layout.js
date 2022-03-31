fa378f754bb7a3dd3b716cbed47d38eedbfe8348
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2022-03-30 21:59:57 -0400

    center the 2021 in review button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -30,11 +30,11 @@ export default class Body extends React.Component {
                     ButtonComponent={CloseOutlined}
                     cookieName="impact-report"
                     font-family = "Segoe UI"
-                    display = "flex"
-                    alignItems = "center"
-                    style={{ background: "#394A74", position: "sticky", top: "0px" }}
-                    buttonStyle={{ background: "none", color:"white", fontSize: "1.25em" }}>
-                    <div className="inverse">
+                    display= "flex"
+                    align-items = "center"
+                    style={{ background: "#394A74", position: "sticky", top: "0px"}}
+                    buttonStyle={{ background: "none", color:"white", fontSize: "1.25em"}}>
+                    <div className="inverse" style={{display: "flex", justifyContent: "center"}}>
                         <Action action={{ label: '2021 in Review', url: "/assets/software-for-love-impact-report.pdf", style: "secondary" }}/>
                     </div>
                 </CookieConsent>
