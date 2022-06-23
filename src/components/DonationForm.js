a3bb31ddeb4e2adbe734f6373c9c7a2a5ffe6002
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-06-23 19:03:25 -0400

    Add componentDidMount to DonationForm component

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -9,56 +9,62 @@ export default class DonationForm extends React.Component {
     };
   }
 
-  handleSetAmount(amount) {
-    return () => {
-      this.setState({...this.state, amount})
-    }
-  }
-  
-  handleSubmit = function (submitEvent) {
-    submitEvent.preventDefault();
-	var value = submitEvent.target.elements.amount.value;
-	const amount = {amount : value}
-	
-	if(value > 0){
-		
-	  fetch('/.netlify/functions/checkout', {
-	    method: 'POST',
-	    headers: {
-		  'Content-Type': 'application/json',
-	    },
-	    body: JSON.stringify(amount),
-	  })
-	  .then(response => response.json())
-	  .then(data => {
-	    console.log('Success:', amount);
-	  })
-	  .catch((error) => {
-	    console.error('Error:', error);
-	  });
-	} else {
-	  alert("Invalid donation amount")	
-	}	
+  // Use this method to get query params and display appropriate message
+  componentDidMount() {
+    const params = new URLSearchParams(window.location.search);
   }
 
-  render () {
+  handleSetAmount(amount) {
+    return () => {
+      this.setState({ ...this.state, amount })
+    }
+  }
+
+  handleSubmit = (submitEvent) => {
+    submitEvent.preventDefault();
+
+    const parsedAmount = parseFloat(this.state.amount);
+
+    if (!Number.isNaN(parsedAmount) && parsedAmount > 0) {
+      fetch('/.netlify/functions/checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          amount: parsedAmount
+        }),
+      })
+        .then(response => response.json())
+        .then(({ data }) => {
+          window.location.replace(data.url);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    } else {
+      alert("Invalid donation amount")
+    }
+  }
+
+  render() {
     return (
       <section className="section bg-gray" style={{ paddingTop: '100px' }}>
         <div className="container container--lg" style={{ display: 'flex' }}>
           <div style={{ display: 'flex', flex: '1' }}>
             <div style={{ maxWidth: '360px' }}>
-              <h3 className="section__title" style={{paddingBottom: '20px'}}>
+              <h3 className="section__title" style={{ paddingBottom: '20px' }}>
                 Help us continue our work by donating to us.
               </h3>
               <div>
-               All donations will go towards any expenses related to maintaining our organization. Any surplus will be donated to charities.
+                All donations will go towards any expenses related to maintaining our organization. Any surplus will be donated to charities.
               </div>
             </div>
           </div>
 
           <div className="flex--center" style={{ display: 'flex', flex: '1' }}>
-            <div style={{ backgroundColor: 'white', width: '500px', borderRadius: '5px', padding: '50px', display: 'flex', flexDirection: 'column'}}>
-              <h3 className="section__title" style={{paddingBottom: '20px'}}>
+            <div style={{ backgroundColor: 'white', width: '500px', borderRadius: '5px', padding: '50px', display: 'flex', flexDirection: 'column' }}>
+              <h3 className="section__title" style={{ paddingBottom: '20px' }}>
                 Support Us!
               </h3>
               <form onSubmit={this.handleSubmit}>
@@ -78,11 +84,11 @@ export default class DonationForm extends React.Component {
                 </div>
                 <div className="form-group">
                   <label htmlFor="amount">Enter Amount</label>
-                  <div style={{position: 'relative'}}>
-                    <span style={{position: 'absolute', fontSize: '16px', top: '50%', transform: 'translateY(-50%)', left: '8px'}}>
+                  <div style={{ position: 'relative' }}>
+                    <span style={{ position: 'absolute', fontSize: '16px', top: '50%', transform: 'translateY(-50%)', left: '8px' }}>
                       $
                     </span>
-                    <input type="number" name="amount" id="amount" placeholder="Amount" value={this.state.amount} onChange={(event) => this.setState({ amount: event.target.value })} style={{width: '50%', paddingLeft: '20px  '}} required />
+                    <input type="number" name="amount" id="amount" placeholder="Amount" value={this.state.amount} onChange={(event) => this.setState({ amount: event.target.value })} style={{ width: '50%', paddingLeft: '20px  ' }} required />
                   </div>
                 </div>
                 <div className="form-submit">
